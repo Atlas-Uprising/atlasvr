19aa6a9d4283ed7e850b7daa75c29a012ac7c321
Collin <collin@hypextech.com>
2022-04-05 15:33:30 -0500

    Should fix voice permission bug

--- garrysmod/addons/vrmod/lua/vrmod/vrmod_ui_chat.lua
+++ garrysmod/addons/vrmod/lua/vrmod/vrmod_ui_chat.lua
@@ -121,7 +121,7 @@ if CLIENT then
 			end
 			button.OnMousePressed = function()
 				if i == 1 then
-					LocalPlayer():ConCommand(LocalPlayer():IsSpeaking() and "-voicerecord" or "+voicerecord")
+					permissions.EnableVoiceChat(not LocalPlayer():IsSpeaking())
 					timer.Simple(0.01,function()
 						chatPanel.button1:SetTextColor(LocalPlayer():IsSpeaking() and Color(0,255,0,255) or Color(255,0,0,255))
 					end)
